0dc7c93f9dd4870a8bcfb53b3468545f9999e08c
kuliITnews <122293667+kuliITnews@users.noreply.github.com>
2023-01-17 10:15:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// header scrolling effect
+
 $(window).on('scroll', function(){
 	if($(window).scrollTop()){
       $('header').addClass('nav-show');
@@ -10,7 +10,6 @@ $(window).on('scroll', function(){
 	   
 })
 
-//hamburger
 const navSlide = () => {
 	 const hamburger = document.querySelector(".hamburger");
 	 const navbar = document.querySelector(".nav-bar");
@@ -20,15 +19,14 @@ const navSlide = () => {
 		
 	 navbar.classList.toggle("nav-active");
 		 
-      //Animation links
+      
 	 navLinks.forEach((link, index) => {
 		if (link.style.animation) {
 			link.style.animation = "";
 		} else {
 			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
 		   }
-		});
-	  //hamburger animation
+		})
 	 hamburger.classList.toggle("toggle");
     }
 	 
